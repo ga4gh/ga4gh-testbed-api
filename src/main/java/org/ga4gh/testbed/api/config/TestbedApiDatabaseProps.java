44375b02f480ce88313c1d9f7fc5a6305301385d
dashrath chauhan <dashrath.chauhan@ga4gh.org>
2024-01-22 11:52:10 +0000

    Added null check with environment variables.

--- src/main/java/org/ga4gh/testbed/api/config/TestbedApiDatabaseProps.java
+++ src/main/java/org/ga4gh/testbed/api/config/TestbedApiDatabaseProps.java
@@ -2,20 +2,28 @@ package org.ga4gh.testbed.api.config;
 
 import org.ga4gh.starterkit.common.config.DatabaseProps;
 
+import java.util.Properties;
+
 public class TestbedApiDatabaseProps extends DatabaseProps {
 
     public TestbedApiDatabaseProps() {
+        super();
         System.out.println("In database config class");
         String databaseUrl = System.getenv("DBHost");
         String databasePort = System.getenv("DBPort");
         String databaseName = System.getenv("DBName");
         String databaseUser = System.getenv("DBUser");
         String databasePassword = System.getenv("DBPass");
-        System.out.println("Database"+databaseUrl);
-        String jdbcUrl = "jdbc:postgresql://" + databaseUrl + ":" + databasePort + "/" + databaseName;
-        setUrl(jdbcUrl);
-        setUsername(databaseUser);
-        setPassword(databasePassword);
+        String jdbcUrl = null;
+
+        if(databaseUrl == null || databaseUser == null || databasePassword == null) {
+            setUrl("jdbc:sqlite:./ga4gh-testbed-api.dev.db");
+        } else {
+            jdbcUrl = "jdbc:postgresql://" + databaseUrl + ":" + databasePort + "/" + databaseName;
+            setUrl(jdbcUrl);
+            setUsername(databaseUser);
+            setPassword(databasePassword);
+        }
         setPoolSize("8");
     }
 }
