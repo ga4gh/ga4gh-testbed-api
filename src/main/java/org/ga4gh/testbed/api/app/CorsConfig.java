cf394dfe2d5c513dba8e1f4ace81ea72cf0cfc69
dashrath chauhan <dashrath.chauhan@ga4gh.org>
2024-06-07 12:30:30 +0100

    Cors configuration.

---
+++ src/main/java/org/ga4gh/testbed/api/app/CorsConfig.java
@@ -0,0 +1,26 @@
+package org.ga4gh.testbed.api.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author dashrath
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dev-testbed.ga4gh.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
